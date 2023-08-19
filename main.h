531ebefb672310d0cb8b724d61030b459c841362
marwannour1 <marwannour20022016@gmail.com>
2023-08-19 14:01:31 -0700

    for changes

--- main.h
+++ main.h
@@ -4,10 +4,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
 
 int _putchar(char c);
+
+/* _printf.c file */
 int _printf(const char *format, ...);
+
 int print_normal_char(char c);
+
 int print_variable(char c, va_list pri);
+
 int print_escape_char(char c);
+
 #endif /* MAIN_H */
