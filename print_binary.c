f5541600ec11d6880444af7d7e8345bd06d81184
marwannour1 <marwannour20022016@gmail.com>
2023-08-22 19:55:24 -0700

    deadline 1

--- print_binary.c
+++ print_binary.c
@@ -5,33 +5,23 @@
  *
  * Return: count of bytes.
  */
-int print_binay(va_list pri)
+int print_binary(va_list pri)
 {
-	unsigned int bin = va_arg(pri, unsigned int);
-	unsigned int temp = bin;
-	unsigned int arr_size = 0;
-	int i;
-	char *ptr;
+	int count = 0;
+	unsigned int numb = va_arg(pri, unsigned int);
+	unsigned int mask = 1 << (sizeof(unsigned int) * 8 - 1);
 
-	if (!bin)
-		return (_putchar('0'));
-	while (temp != 0)
+	if (numb == 0)
 	{
-		temp /= 2;
-		arr_size++;
+		_putchar('0');
+		return (1);
 	}
-	ptr = malloc(sizeof(*ptr) * arr_size);
-	if (ptr == NULL)
-		return (NULL);
-	for (i = arr_size; i >= 0; i--)
+
+	while (mask != 0)
 	{
-		ptr[i] = '0' + (bin % 2);
-		bin /= 2;
+		((numb & mask) != 0) ?  _putchar('1') :  _putchar('0');
+		count++;
+		mask >>= 1;
 	}
-	for (i = 0; i < arr_size - 1; i++)
-	{
-		_putchar(ptr[i]);
-	}
-	_putchar('0' + (temp2 % 2));
-	return (arr_size);
+	return (count);
 }
